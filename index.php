54e3a8b480892ddcb3d736e94003277e9da32213
Mnde81 <minubis@gmail.com>
2024-12-05 07:54:27 +0200

    updated file

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include "partials/header.php";
 include "partials/notifications.php";
 include "config/Database.php";
 include "classes/Task.php";
+session_start();
 
 $database = new Database();
 
@@ -12,19 +13,50 @@ $db = $database->connect();
 $todo = new Task($db);
 
 
+
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     if(isset($_POST['add_task'])){
         $todo->task = $_POST['task'];
 
         $todo->create();
+        $_SESSION['message'] = "Task added successfully";
+        $_SESSION['msg_type'] = "success";
+
 
         
+    } elseif (isset($_POST['complete_task'])) {
+        $todo->complete($_POST['id']);
+        $_SESSION['message'] = "Task marked completed";
+        $_SESSION['msg_type'] = "success";
+    } elseif (isset($_POST['undo_complete_task'])) {        
+        $todo->undoComplete($_POST['id']);
+        $_SESSION['message'] = "Task marked incomplete";
+        $_SESSION['msg_type'] = "success";
+    } elseif (isset($_POST['delete_task'])) {
+        $todo->delete($_POST['id']);
+        $_SESSION['message'] = "Task deleted";
+        $_SESSION['msg_type'] = "success";
+             
     }
 }
 
-
+$tasks = $todo->read();
 ?>
+
+<?php if(isset($_SESSION['message'])): ?>
+
+    <div class="notification-container <?php echo isset($_SESSION['message']) ? "show" : '';?>">
+        <div class="notification <?php echo $_SESSION['msg_type']; ?>">
+            <?php echo $_SESSION['message']; ?>
+            <?php unset($_SESSION['message']); ?>
+        </div>
+        
+    </div>
+
+
+<?php endif; ?>
 
 <!-- Main Content Container -->
 <div class="container">
@@ -38,48 +70,45 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <!-- Display Tasks -->
     <ul>
+        <?php while($task = $tasks->fetch_assoc()): ?>
         <li class="completed">
-            <span class="completed">Sample Task</span>
+            <span class="<?php echo $task['is_completed'] ? 'completed' : ''; ?>"><?php echo $task['task']; ?></span>
             <div>
+                <?php if(!$task['is_completed']): ?>
                 <!-- Complete Task -->
                 <form method="POST" style="display:inline;">
-                    <input type="hidden" name="id" value="1">
+                    <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
                     <button class="complete" type="submit" name="complete_task">Complete</button>
                 </form>
+
+                <?php else: ?>
                 
                 <!-- Undo Completed Task -->
                 <form method="POST" style="display:inline;">
-                    <input type="hidden" name="id" value="1">
+                    <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
                     <button class="undo" type="submit" name="undo_complete_task">Undo</button>
                 </form>
 
+                <?php endif; ?>
+
                 <!-- Delete Task -->
-                <form method="POST" style="display:inline;">
-                    <input type="hidden" name="id" value="1">
+                <form onsubmit="return confirmDelete()" method="POST" style="display:inline;">
+                    <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
                     <button class="delete" type="submit" name="delete_task">Delete</button>
                 </form>
             </div>
         </li>
 
-        <li>
-            <span>Another Task</span>
-            <div>
-                <!-- Complete Task -->
-                <form method="POST" style="display:inline;">
-                    <input type="hidden" name="id" value="2">
-                    <button class="complete" type="submit" name="complete_task">Complete</button>
-                </form>
-
-                <!-- Delete Task -->
-                <form method="POST" style="display:inline;">
-                    <input type="hidden" name="id" value="2">
-                    <button class="delete" type="submit" name="delete_task">Delete</button>
-                </form>
-            </div>
-        </li>
+        
+        <?php endwhile; ?>
     </ul>
 </div>
 
+<script>
+    function confirmDelete() {
+        return confirm("Are you sure you want to delete?");
+    }
+</script>
 
 <?php
 include "partials/footer.php";
